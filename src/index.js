a85d78f51b0cbad14a60f59ad1f04c9736876643
Malick NDIAYE <98133571+MalicknND@users.noreply.github.com>
2023-03-12 13:45:23 +0100

    GET and POST category🎆

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
+const categoryRoute = require('./routes/categories');
 
 app.use(express.json());
 
@@ -20,6 +21,7 @@ mongoose
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
+app.use('/api/categories', categoryRoute);
 
 //Méthod launch app
 app.listen(process.env.PORT, function () {
