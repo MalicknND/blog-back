c68dd21ddf20903fc1a4c0979f3520e8ffae37aa
Malick NDIAYE <98133571+MalicknND@users.noreply.github.com>
2023-03-12 14:04:20 +0100

    upload image with multer

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
 const categoryRoute = require('./routes/categories');
+const multer = require('multer');
 
 app.use(express.json());
 
@@ -17,6 +18,19 @@ mongoose
     console.log('successfully connect to database');
   })
   .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, 'image.jpeg');
+  },
+});
+const upload = multer({ storage: storage });
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  res.status(200).json('File has been uploaded');
+});
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
